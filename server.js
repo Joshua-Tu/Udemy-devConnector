4e3b23e75e12a551349da65031415877fc088fed
Joshua Tu <tjh8523@gmail.com>
2019-07-06 12:45:39 +1000

    routes created

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 // get a very simple express server up and running
 const express = require("express");
+const connectDB = require("./config/db");
 // initialize app variable with express
 const app = express();
 //if there's no environment variable for port set, it will default to 5000
 const PORT = process.env.PORT || 5000;
+//Connect Database
+//connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
+
+//Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
